f3cf63a9e1186f72f39750e88adff5325012f9da
cjCondon <152666137+cjCondon@users.noreply.github.com>
2024-02-18 17:40:54 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "tokenizer.c"
+#include "inputProcessor.c"
 
 #define MAX 1024
 
@@ -12,6 +12,6 @@ int main(){
     
     printf("\n");
 
-    tokenize(input);
+    processInput(input);
     return 0;
 }
